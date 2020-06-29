7ffd717ae3ae85a968be242035ffc81a14f11558
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2020-06-29 14:53:57 -0400

    Adding new endpoints

--- src/main/java/com/microservice/demo/controller/CurrencyExchangeController.java
+++ src/main/java/com/microservice/demo/controller/CurrencyExchangeController.java
@@ -1,21 +1,22 @@
 package com.microservice.demo.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.demo.entity.ExchangeValue;
-import com.microservice.demo.repository.ExchangeValueRepository;
 import com.microservice.demo.service.CurrencyExchangeService;
 
 @RestController
 public class CurrencyExchangeController {
-
-	private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	Environment environment;
@@ -23,10 +24,28 @@ public class CurrencyExchangeController {
 	@Autowired
 	CurrencyExchangeService currencyExchangeService;
 	
-	
-	
 	@GetMapping("/currency-exchange/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		return currencyExchangeService.getExchangeValue(from, to);
+	}
+	
+	@PostMapping("/currency-exchange/rate")
+	public void createExchangeValue(@RequestBody ExchangeValue exchangeValue) {
+		currencyExchangeService.createExchangeValue(exchangeValue);
+	}
+	
+	@PutMapping("/currency-exchange/rate")
+	public void updateExchangeValue(@RequestBody ExchangeValue exchangeValue) {
+		currencyExchangeService.updateExchangeValue(exchangeValue);
+	}
+	
+	@DeleteMapping("/currency-exchange/{from}/to/{to}")
+	public void deleteExchangeValue(@PathVariable String from, @PathVariable String to) {
+		currencyExchangeService.deleteExchangeValue(from, to);
+	}
+	
+	@GetMapping("/currency-exchange/rates")
+	public List<ExchangeValue> getAllExchangeValue() {
+		return currencyExchangeService.getAllExchangeValue();
 	}
 }
--- src/main/java/com/microservice/demo/service/CurrencyExchangeService.java
+++ src/main/java/com/microservice/demo/service/CurrencyExchangeService.java
@@ -1,11 +1,12 @@
 package com.microservice.demo.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.microservice.demo.controller.CurrencyExchangeController;
 import com.microservice.demo.entity.ExchangeValue;
 import com.microservice.demo.repository.ExchangeValueRepository;
 
@@ -22,6 +23,33 @@ public class CurrencyExchangeService {
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 		logger.info("Exchange Rate/Value found from MySql DB {} ", exchangeValue);
 		return exchangeValue;
+	}
+
+	public void createExchangeValue(ExchangeValue exchangeValue) {
+		logger.info("Creating the Exchange Value in MySql DB {}", exchangeValue);
+		exchangeValueRepository.save(exchangeValue);
+		logger.info("Created the Exchange Value in MySql DB {} ", exchangeValue);
+	}
+
+	public void updateExchangeValue(ExchangeValue exchangeValue) {
+		logger.info("Updating the Exchange Value in MySql DB {}", exchangeValue);
+		exchangeValueRepository.save(exchangeValue);
+		logger.info("Updated the Exchange Value in MySql DB {} ", exchangeValue);
+	}
+
+	public void deleteExchangeValue(String from, String to) {
+		logger.info("Deleting Exchange Rate/Value for {} to {} ", from, to);
+		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+		logger.info("Exchange Rate/Value found from MySql DB {} ", exchangeValue);
+		exchangeValueRepository.delete(exchangeValue);
+		logger.info("Deleted Exchange Rate/Value from MySql DB {} ", exchangeValue);
+	}
+
+	public List<ExchangeValue> getAllExchangeValue() {
+		logger.info("Retrieving all the Exchange Value from MySql DB");
+		List<ExchangeValue> exchangeValues = exchangeValueRepository.findAll();
+		logger.info("Retrieved all the Exchange Value from MySql DB {}", exchangeValues);
+		return exchangeValues;
 	}
 
 }
